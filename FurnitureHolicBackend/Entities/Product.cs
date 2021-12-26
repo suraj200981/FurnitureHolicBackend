500f2c6525323c5b7e6e20635084759a55f5dec2
suraj200981 <suraj200981@yahoo.co.uk>
2021-12-26 13:29:37 +0000

    User and product models created

---
+++ FurnitureHolicBackend/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FurnitureHolicBackend.Entities
+{
+    public class Product
+    {
+    }
+}
---
+++ FurnitureHolicBackend/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FurnitureHolicBackend.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+
+        //forming 1 to many relationship with Product (1 user can have many products)
+        public ICollection<Product> products { get; set; }
+    }
+}
