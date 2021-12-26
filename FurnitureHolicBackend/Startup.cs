51882440c60b5dfcc091ad806cb513c321fb5d52
suraj200981 <suraj200981@yahoo.co.uk>
2021-12-26 13:38:16 +0000

    All NuGet packages needed installed

--- FurnitureHolicBackend/Startup.cs
+++ FurnitureHolicBackend/Startup.cs
@@ -23,7 +23,7 @@ namespace FurnitureHolicBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
